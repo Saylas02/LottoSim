3327dc3294433bad575d3b77d3f00c78116326e0
hermessil <Silas.Hermes02@gmail.com>
2025-11-07 15:23:09 +0100

    add features (silent mode, more stats)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random as r
+import time as t
 
 def generate_random_number(low, high) -> int:
     return r.randint(low, high)
@@ -20,7 +21,8 @@ def check_matches(con, mas)-> int:
             m += 1
     return m
 
-def game_process( bonus:bool ) -> None:
+def game_process( bonus:bool, silent:bool ) -> None:
+    start_time = t.time()
     ticket_amount:int = 0
     game_bonus_num:int = 0
     match_count = [0, 0, 0, 0, 0, 0]
@@ -31,9 +33,11 @@ def game_process( bonus:bool ) -> None:
         game_bonus_num = generate_random_number(1,6)
 
     if not bonus:
-        print(f"Master ticket: {master_ticket}")
+        if not silent:
+            print(f"Master ticket: {master_ticket}")
     elif bonus:
-        print(f"Master ticket / Bonus Number: {master_ticket}/{game_bonus_num}")
+        if not silent:
+            print(f"Master ticket / Bonus Number: {master_ticket}/{game_bonus_num}")
 
     while True:
         ticket_amount += 1
@@ -56,24 +60,31 @@ def game_process( bonus:bool ) -> None:
             else:
                 ticket_bonus_num = generate_random_number(1,6)
                 if game_bonus_num == ticket_bonus_num:
-                    print(f"Correct bonus: {ticket_bonus_num}, master: {game_bonus_num}")
+                    if not silent:
+                        print(f"Correct bonus: {ticket_bonus_num}, master: {game_bonus_num}")
                     break
                 else:
-                    print(f"Wrong bonus: {ticket_bonus_num}, master: {game_bonus_num}")
+                    if not silent:
+                        print(f"Wrong bonus: {ticket_bonus_num}, master: {game_bonus_num}")
 
+    end_time = t.time()
+    runtime = round(end_time - start_time)
 
+    tickets_per_sec =  ticket_amount // runtime
 
     price = ticket_amount * 1.20
     price = "${:,.2f}".format(price)
 
-    print("")
-    print("")
 
-    print("*************** S T A T S ***************")
-    print(f"Total tickets bought: {ticket_amount}")
-    print(f"Total matches overview: {match_count}")
-    print(f"Total ticket price (1,20€): {price}")
-    print("*****************************************")
+    if not silent:
+        print("")
+        print("*************** S T A T S ***************")
+        print(f"Total runtime: {runtime} seconds")
+        print(f"Total tickets per second: {tickets_per_sec}")
+        print(f"Total tickets bought: {ticket_amount}")
+        print(f"Total matches overview: {match_count}")
+        print(f"Total ticket price (1,20€): {price}")
+        print("*****************************************")
 
 if __name__ == "__main__":
-    game_process( bonus = True )
+    game_process( bonus = False, silent = True )
